75b32a0a92362863c4d2ba03d77d79e3cbaa083b
Samirkeddache <samir.keddache05@gmail.com>
2022-03-26 16:11:55 -0400

    updated First file

---
+++ src/day0/First.java
@@ -0,0 +1,14 @@
+package day0;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("Working...");
+
+    }
+    // add some cool code
+
+    //extra line 1
+    //extra line 2
+    //extra line 3
+}
